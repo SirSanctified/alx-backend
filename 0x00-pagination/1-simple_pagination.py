35452eb8a80852d502c3b7f901447ed102c070c9
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-04-16 21:28:22 +0200

    Implement simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+Simple pagination
+"""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(self, page: int, page_size: int) -> tuple:
+        """
+        return a tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters.
+        """
+        start_index = (page - 1) * page_size
+        end_index = page * page_size
+        return (start_index, end_index)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        return the appropriate page of the dataset
+        (i.e. the correct list of rows).
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        start, end = self.index_range(page, page_size)
+        if start >= len(self.dataset()):
+            return []
+        return self.dataset()[start:end]
+
+
+if __name__ == '__main__':
+    server = Server()
+
+    try:
+        should_err = server.get_page(-10, 2)
+    except AssertionError:
+        print("AssertionError raised with negative values")
+
+    try:
+        should_err = server.get_page(0, 0)
+    except AssertionError:
+        print("AssertionError raised with 0")
+
+    try:
+        should_err = server.get_page(2, 'Bob')
+    except AssertionError:
+        print("AssertionError raised when page and/or page_size are not ints")
+
+    print(server.get_page(1, 3))
+    print(server.get_page(3, 2))
+    print(server.get_page(3000, 100))
