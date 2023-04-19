d46d612c123c9aecba36dfc7c41db78c7ce50377
SirSanctified <mambambopritchard@gmail.com>
2023-04-19 20:38:33 +0200

    Implement basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,18 +6,17 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     """BasicCache class"""
-    
+
     def put(self, key, item):
         """Add an item in the cache"""
         if key and item:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """Get an item by key"""
         if key in self.cache_data:
             return self.cache_data[key]
         return None
-
 
 
 if __name__ == '__main__':
@@ -37,4 +36,3 @@ if __name__ == '__main__':
     my_cache.put("A", "Street")
     my_cache.print_cache()
     print(my_cache.get("A"))
-
